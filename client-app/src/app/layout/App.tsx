5694bbd997cb0f6033cfeafe6b92efd2f0e63c76
Sanchay Laud <sanchay.laud@softude.com>
2024-09-30 18:39:49 +0530

    MobX observer

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,18 @@
 
 import { useEffect, useState } from 'react'
-import { Container } from 'semantic-ui-react';
+import { Button, Container } from 'semantic-ui-react';
 import { Activity } from '../models/activity';
 import Navbar from './Navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import {v4 as uuid} from 'uuid'
 import agent from '../api/agent';
 import LoadingComponent from './LoadingComponent';
+import { useStore } from '../stores/store';
+import { observer } from 'mobx-react-lite';
 
 function App() {
+const {activityStore} = useStore();
+
   const [activities, setActivities] = useState<Activity[]>([]);
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined); 
   const [editMode, setEditMode] = useState(false); 
@@ -80,6 +84,8 @@ function App() {
       <>
         <Navbar openForm = {handleFormOpen}/>
         <Container style={{ marginTop: '7em' }}>
+          <h2>{activityStore.title}</h2>
+          <Button content='Add exclamation!' positive onClick={activityStore.setTitle }/>
          <ActivityDashboard 
          activities={activities} 
          selectedActivity = {selectedActivity}
@@ -98,4 +104,4 @@ function App() {
   );
 }
 
-export default App
+export default observer(App)
--- client-app/src/features/main.tsx
+++ client-app/src/features/main.tsx
@@ -2,13 +2,16 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '../app/layout/App.tsx'
 
-import 'semantic-ui-css/semantic.min.css' 
+import 'semantic-ui-css/semantic.min.css'
 import '../app/layout/styles.css'
+import { store, StoreContext } from '../app/stores/store.ts'
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
   </StrictMode>,
 )
